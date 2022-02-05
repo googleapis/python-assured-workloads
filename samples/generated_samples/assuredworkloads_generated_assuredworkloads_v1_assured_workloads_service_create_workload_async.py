aa0ab5e257e5f3a957ff210366bf563c8629f33a
Anthonios Partheniou <partheniou@google.com>
2022-02-05 17:09:34 +0000

    fix package name in samples/generated_samples

--- samples/generated_samples/assuredworkloads_generated_assuredworkloads_v1_assured_workloads_service_create_workload_async.py
+++ samples/generated_samples/assuredworkloads_generated_assuredworkloads_v1_assured_workloads_service_create_workload_async.py
@@ -20,7 +20,7 @@
 # It may require modifications to work in your environment.
 
 # To install the latest published package dependency, execute the following:
-#   python3 -m pip install google-cloud-assuredworkloads
+#   python3 -m pip install google-cloud-assured-workloads
 
 
 # [START assuredworkloads_generated_assuredworkloads_v1_AssuredWorkloadsService_CreateWorkload_async]
